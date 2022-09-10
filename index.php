beb09beb9758671f60ae792bf07f90517a007536
roootmrx <mr.root003@gmail.com>
2022-09-10 09:29:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
  
 <html>
 <body>
-
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0001.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0002.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0003.gif"/>
-</br>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0009.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0007.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0006.gif"/>
-</br>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0010.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0012.gif"/>
-<img src="http://d205bpvrqc9yn1.cloudfront.net/0014.gif"/>
+<p>test</p>
+ </body>
 </html>
